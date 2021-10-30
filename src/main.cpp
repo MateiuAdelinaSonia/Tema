c67d0f297fc099d1e9cc0d6ee40d48e65217ed7b
MateiuAdelinaSonia <adelinasonia.mateiu@yahoo.com>
2021-10-30 23:30:57 +0300

    Implemented reading from a database file

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <map>
 #include "car.cpp"
+#define CURENT_USER "admin"
+#define DATABASE_NAME "db.txt"
 
-int main()
+std::map<int, Car*> carsArray;
+
+void readFromDatabase() 
 {
-    std::string currentUser = "admin";
+    std::ifstream database(DATABASE_NAME);
+    int carId;
     std::string color;
     std::string mark;
-    int carNumber;
+    std::string carNumber;
     double power;
     int doorNumber;
     int year;
     bool sport;
     bool automaticCar;
 
-    std::cout << "Car color:" << std::endl;
-    std::cin >> color;
-    std::cout << "Car mark:" << std::endl;
-    std::cin >> mark;
-    std::cout << "Car number:" << std::endl;
-    std::cin >> carNumber;
-    std::cout << "Car power:" << std::endl;
-    std::cin >> power;
-    std::cout << "Car doorNumber:" << std::endl;
-    std::cin >> doorNumber;
-    std::cout << "Car year:" << std::endl;
-    std::cin >> year;
-    std::cout << "Sport car:" << std::endl;
-    std::cin >> sport;
-    std::cout << "Automatic car:" << std::endl;
-    std::cin >> automaticCar;
+    while (database >> carId >> color >> mark >> carNumber >> power >> doorNumber >> year >> sport >> automaticCar) 
+    {
+        Car *carData = new Car(color, mark, carNumber, power, doorNumber, year, sport, automaticCar);
+        carsArray.insert(std::make_pair(carId, carData));
+    }
+}
 
-    Car *car1 = new Car(color, mark, carNumber, power, doorNumber, year, sport, automaticCar);
-    car1->setCreator(currentUser);
-    car1->printCar();
+int main()
+{
+    readFromDatabase();
+
+    carsArray[1]->setCreator(CURENT_USER);
+    carsArray[1]->printCar();
 
     return 0;
 }
